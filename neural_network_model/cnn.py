d187e2ee9e9e42115cf6ea70e916ddee512e951b
navid <navid.falah7@gmail.com>
2024-02-16 19:12:22 +0330

    cnn prints removed

--- neural_network_model/cnn.py
+++ neural_network_model/cnn.py
@@ -16,26 +16,13 @@ class CNNNetwork(nn.Module):
             nn.ReLU(),
         )
         self.flatten = nn.Flatten()
-        # Correct the input feature size of the first fully connected layer to match the actual flattened size
         self.fc_layers = nn.Sequential(
-            nn.Linear(128 * 19 * 1, 512),  # Adjusted based on the expected output size
+            nn.Linear(128 * 19 * 1, 512),
             nn.ReLU(),
-            nn.Linear(512, 1)  # Output a single value for regression
+            nn.Linear(512, 1)
         )
     def forward(self, x):
-        print(f"Input size: {x.size()}")
         x = self.conv_layers(x)
-        print(f"Input size: {x.size()}")
         x = self.flatten(x)
-        print(f"Input size: {x.size()}")
         x = self.fc_layers(x)
-        print(f"Input size: {x.size()}")
         return x
-    
-# if __name__ == "__main__":
-#     cnn = CNNNetwork()
-#     cnn.cuda()  # Assuming you are using a CUDA-enabled GPU
-#     # Calculate the size of the flattened output after the conv and pooling layers
-#     # This is necessary to define the first Linear layer properly
-#     # You might need to adjust the calculation based on your actual input size and architecture
-#     summary(cnn, (1, 64, 44))
