8f30dbae6b8f3b9b584ab4ab7914e334e072654d
navid <navid.falah7@gmail.com>
2024-02-25 10:42:07 +0330

    new model trained and train output config changed

--- neural_network_model/constants.py
+++ neural_network_model/constants.py
@@ -1,7 +1,7 @@
 # constants
 ANNOTATIONS_FILE = "/home/navid/Desktop/Spotify-Popularity-Estimator-Pytorch/data_spotify/all_spotify_data_output.csv"
 AUDIO_DIR = "/home/navid/Desktop/Spotify-Popularity-Estimator-Pytorch/data_spotify/wav"
-TRAIN_OUTPUT = "/home/navid/Desktop/Spotify-Popularity-Estimator-Pytorch/outputs/song.pth"
+TRAIN_OUTPUT = "/home/navid/Desktop/Spotify-Popularity-Estimator-Pytorch/outputs/song_new.pth"
 BATCH_SIZE = 128
 EPOCHS = 10
 LEARNING_RATE = 0.001
